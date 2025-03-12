68af9af972204c849d891bbe78f259c4ce324318
Talyta Perrotti <talyytta@gmail.com>
2025-03-11 21:21:19 -0300

    Projeto inicial

---
+++ src/main/java/fiap/com/br/CP1_2_Java/Cp12JavaApplication.java
@@ -0,0 +1,13 @@
+package fiap.com.br.CP1_2_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cp12JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cp12JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fiap/com/br/CP1_2_Java/Cp12JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fiap.com.br.CP1_2_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp12JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
